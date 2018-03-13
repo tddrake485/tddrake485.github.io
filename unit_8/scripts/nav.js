c34a66f631fb615633cb7f6432962ea8b455d221
Tom Drake <tom.drake@cloudcraze.com>
2018-03-12 21:31:08 -0500

    Unit 8: responsive page
    Adding responsive CSS styles, smaller profile pic for smaller screens, and TOC toggle jS

---
+++ unit_8/scripts/nav.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#site-nav").show();
+});
+
+$(document).ready(function(){
+    $("#toggle-mobile-nav").click(function(){
+        $("#site-nav").toggle();
+    });
+});
